4ea46438f51c44a7e4e09c165d78b5ae3c743805
plausibly <pouyaghi123@gmail.com>
2021-09-19 21:39:33 -0400

    Add NGINX reverse proxying to the API subdomain

--- server/server.ts
+++ server/server.ts
@@ -8,11 +8,11 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req: express.Request, res: express.Response) => {
+app.get('/v1/hello', (req: express.Request, res: express.Response) => {
     res.send({ express: 'Hello From Express' });
 });
 
-app.post('/api/world', (req: express.Request, res: express.Response) => {
+app.post('/v1/world', (req: express.Request, res: express.Response) => {
     console.log(req.body);
     res.send(
         `I received your POST request. This is what you sent me: ${req.body.post}`,

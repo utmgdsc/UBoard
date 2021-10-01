44d42f6c38a5e418b85d1a36c5663688c2ada3ed
plausibly <pouyaghi123@gmail.com>
2021-10-01 17:13:28 -0400

    Remove URL used for testing

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-    DB_URL: process.env.DATABASE_URL || "postgres://vlynoaoa:ZPD-xj3wMzTFF4pKggWYU_dmZUeyYOJ9@fanny.db.elephantsql.com/vlynoaoa", // testing URL doesnt matter
+    DB_URL: process.env.DATABASE_URL
     DIALECT: "postgres"
   
 };

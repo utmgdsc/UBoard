676f0f8525198ef024e3ecda5620f238e3010bc2
danielduong8 <daniel.duong@mail.utoronto.ca>
2021-11-04 21:09:14 -0400

    Update URIs of transactional emails to be more concise

--- server/services/emailService.ts
+++ server/services/emailService.ts
@@ -39,7 +39,7 @@ export default class EmailService {
     lastName: string,
     emailAddress: string
   ): Promise<boolean> {
-    const confirmURL = `${this.apiRoute}/users/confirmation/c=${confToken}`; // this will be our route
+    const confirmURL = `${this.apiRoute}/users/confirm/?c=${confToken}`; // this will be our route
     const subjectLine = "UBoard - Confirm your Email Address";
 
     const body = `Thank you for signing up to UBoard, ${firstName} ${lastName}.
@@ -63,7 +63,7 @@ export default class EmailService {
     userName: string,
     emailAddress: string
   ): Promise<boolean> {
-    const resetURL = `${this.apiRoute}/users/password-reset/r=${confToken}`;
+    const resetURL = `${this.apiRoute}/users/reset/?r=${confToken}`;
     const subjectLine = "UBoard - Password Reset Requested";
     const body = `Hello,  ${firstName} ${lastName}.
         A password reset has been requested for the account with username: ${userName}. To reset your password, click the link below. 

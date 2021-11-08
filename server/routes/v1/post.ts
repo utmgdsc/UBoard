4a4d6acf9095f5024a9a754ea6b46b9a05b332ad
Andy Chrzaszcz <andy9775@gmail.com>
2021-11-08 12:47:59 -0500

    Create CRUD endpoints for posts. (#33)
    * Create CRUD endpoints for posts.
    
    Create the CRUD controller to:
    - getPosts
    - getPost (by ID)
    - delete post
    - update post
    - upvote post
    - report post
    
    Connects up all of the controller methods to the router.
    
    * Use helper function to get the current user when creating a post.
    
    * Update logic to check for user id when deleting and updating a post.
    
    * Address PR feedback.
    
    Make the getPosts endpoint message clear when missing `limit` or `offset` and keep res data sending
    consistent with remainder of routes.

---
+++ server/routes/v1/post.ts
@@ -0,0 +1,81 @@
+import express, {Request, Response} from 'express';
+import db from '../../models';
+import PostController from '../../controllers/v1/post';
+import {getAuthUser} from '../../middleware/auth';
+
+const postRouter = express.Router();
+const postController = new PostController(db.User);
+
+postRouter.get('', async (req: Request, res: Response) => {
+  const limit = req.body.limit;
+  const offset = req.body.offset;
+  if (!limit || !offset) {
+    return res.status(400).json({
+      code: 400,
+      message: `Missing ${!limit ? 'limit' : ''} ${!limit && !offset ? 'and' : ''} ${
+        !offset ? 'offset' : ''
+      }`,
+    });
+  }
+
+  const result = await postController.getPosts(limit, offset);
+  return res.status(result.status).json(result);
+});
+
+postRouter.get('/:postid', async (req: Request, res: Response) => {
+  const result = await postController.getPost(req.params.postid);
+  res.status(result.status).json(result);
+});
+
+postRouter.delete('/:postid', async (req: Request, res: Response) => {
+  try {
+    const result = await postController.deletePost(getAuthUser(res).id, req.params.postid);
+    res.status(result.status).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+postRouter.put('/:postid/upvote', async (req: Request, res: Response) => {
+  const result = await postController.upVote(req.params.postid);
+  res.status(result.status);
+});
+
+postRouter.put('/:postid/report', async (req: Request, res: Response) => {
+  const result = await postController.report(req.params.postid);
+  res.status(result.status);
+});
+
+postRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const result = await postController.createPost(
+      getAuthUser(res).id,
+      req.body.title,
+      req.body.body,
+      req.body.location,
+      req.body.capacity
+    );
+    res.status(result.status).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+postRouter.put('/:postid', async (req: Request, res: Response) => {
+  try {
+    const result = await postController.updatePost(
+      getAuthUser(res).id,
+      req.params.postid,
+      req.body.title,
+      req.body.body,
+      req.body.location,
+      req.body.capacity
+    );
+
+    res.status(result.status).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+export default postRouter;

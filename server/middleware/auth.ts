4d08555bdc97e3b8ed662728b94a9e0657afbcff
Andy Chrzaszcz <andy9775@gmail.com>
2021-11-10 17:55:15 -0500

    Enable the auth middleware.
    Adds the ability for the auth middleware to skip certain paths and enables it application wide,
    except for user authentication paths.

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -27,8 +27,20 @@ export function getAuthUser(res: Response): User {
   return res.locals.user;
 }
 
-function auth(userRepo: typeof User) {
+/**
+ * Checks if the user is authenticated and if so, adds the user to the locals object.
+ *
+ * `skipPaths` takes a list of paths which shouldn't be checked by the auth middleware.
+ * @param userRepo Used to query for user info.
+ * @param skipPaths A list of paths which should be skipped.
+ * @returns The next handler function.
+ */
+function auth(userRepo: typeof User, skipPaths: string[] = []) {
   return async (req: Request, res: Response, next: NextFunction) => {
+    if (skipPaths.map(pathRegex => req.path.match(pathRegex)).some(match => match)) {
+      return next();
+    }
+
     try {
       const token = req.cookies.token;
 

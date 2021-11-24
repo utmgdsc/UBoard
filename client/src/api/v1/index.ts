9045807387d5d0abe34f16ee71f6c4fc8b1b8a20
plausibly <pouyaghi123@gmail.com>
2021-11-24 16:26:50 -0500

    Fix merge conflict with index.ts

--- client/src/api/v1/index.ts
+++ client/src/api/v1/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import { Post } from 'models/post';
 import { User } from 'models/user';
+import { UserAttributes } from 'models/user';
 
 export type PostUser = Post & {
   User: { id: string; firstName: string; lastName: string };
@@ -25,14 +26,21 @@ export default class ServerApi {
     this.api = axios.create({ baseURL: `api/v1` });
   }
 
-  protected async post<Type>(path: string, form: Type) {
+  protected async post<RequestDataType, ResponseType>(
+    path: string,
+    form: RequestDataType
+  ): Promise<{ status: number; data: ResponseType }> {
     try {
       const response = await this.api.post(path, form);
-      return { response: response };
+      return { status: response.status, data: response.data };
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        return { error: error };
+        return {
+          status: error.response?.status as number,
+          data: error.response?.data,
+        };
       }
+      throw new ApiError(`Could not post to path: ${path}`);
     }
   }
 
@@ -126,11 +134,10 @@ export default class ServerApi {
     firstName: string;
     lastName: string;
   }) {
-    return await this.post('/users/signup', form);
-  }
-
-  async signIn(form: { userName: string; password: string }) {
-    return await this.post('/users/signin', form);
+    return await this.post<typeof form, { message: string }>(
+      '/users/signup',
+      form
+    );
   }
 
   async signOut() {
@@ -143,5 +150,12 @@ export default class ServerApi {
 
   async reportPost(postID: string) {
     return await this.put<{ status: number }>(`/posts/${postID}/report`);
+  }
+
+  async signIn(form: { userName: string; password: string }) {
+    return await this.post<
+      typeof form,
+      { message: string; result?: UserAttributes }
+    >('/users/signin', form);
   }
 }

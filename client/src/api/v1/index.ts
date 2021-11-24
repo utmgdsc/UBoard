97424e881c0d87337caf5200c3b252bb9b769502
danielduong8 <59457434+danielduong8@users.noreply.github.com>
2021-11-24 16:16:04 -0500

    Add generic typing for client api handler, update related files (#55)
    * Add generic typing for client api handler, update related files
    
    - Add generic return type for client api handler
      - Update logic in sign in and sign up components accordingly.
      - Update auth container tests
    
    * Update README instructions, fix typing in index.ts
    
    * Fix sign in and sign up status logic

--- client/src/api/v1/index.ts
+++ client/src/api/v1/index.ts
@@ -1,4 +1,8 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios';
+
+import { UserAttributes } from 'models/user';
+
+export class ApiError extends Error {}
 
 export default class ServerApi {
   protected api: AxiosInstance;
@@ -7,14 +11,21 @@ export default class ServerApi {
     this.api = axios.create({ baseURL: `api/v1` });
   }
 
-  protected async post<Type>(path: string, form: Type) {
+  protected async post<RequestDataType, ResponseType>(
+    path: string,
+    form: RequestDataType
+  ): Promise<{ status: number; data: ResponseType }> {
     try {
       const response = await this.api.post(path, form);
-      return { response: response };
+      return { status: response.status, data: response.data };
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        return { error: error };
+        return {
+          status: error.response?.status as number,
+          data: error.response?.data,
+        };
       }
+      throw new ApiError(`Could not post to path: ${path}`);
     }
   }
 
@@ -25,10 +36,16 @@ export default class ServerApi {
     firstName: string;
     lastName: string;
   }) {
-    return await this.post("/users/signup", form);
+    return await this.post<typeof form, { message: string }>(
+      '/users/signup',
+      form
+    );
   }
 
   async signIn(form: { userName: string; password: string }) {
-    return await this.post("/users/signin", form);
+    return await this.post<
+      typeof form,
+      { message: string; result?: UserAttributes }
+    >('/users/signin', form);
   }
 }

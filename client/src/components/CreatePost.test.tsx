45451fe020d2562b70e031965a710066bf7eaa67
gurvirsasan <70930457+gurvirsasan@users.noreply.github.com>
2021-11-27 21:56:29 -0500

    Create post api (#62)
    * Implemented a dialog option along with a back button. Also, added tests
    
    * Adding api directory
    
    * Remvoed  and added package.json
    
    * only allow numeric inputs for capacity and other small nits
    
    * removed api/v1/index.ts
    
    * Update CreatePost to use the new API class. Added Snackbar for alerts.
    
    * Fixed  type
    
    * Changed type to fix error
    
    * Removed index.test.tssx
    
    * Changed show alert under finally of the api call
    
    * Changing status code
    
    * Emptying fields upon closing
    
    * Grey out Submit until 25 characters are provided for post description
    
    * Fixed test error
    
    * Changed accessing  during testing from placeholder text, made capacity into an integer
    
    * Changed capacity default value from -1 to 0
    
    Co-authored-by: danielduong8 <daniel.duong@mail.utoronto.ca>

--- client/src/components/CreatePost.test.tsx
+++ client/src/components/CreatePost.test.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import CreatePost from "./CreatePost";
-import { unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
-import newImage from "../assets/background.jpg";
+import React from 'react';
+import CreatePost from './CreatePost';
+import { unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 
 let container: HTMLElement | null = null;
 beforeEach(() => {
   // setup a DOM element as a render target
-  container = document.createElement("div");
+  container = document.createElement('div');
   document.body.appendChild(container);
   act(() => {
     render(<CreatePost />);
   });
+  screen.getByTestId('newPostButton').click();
 });
 
 afterEach(() => {
@@ -25,34 +25,42 @@ afterEach(() => {
   }
 });
 
-describe("verifying launch of create post component", () => {
-  it("shows the preview button as disabled", () => {
+describe('verifying launch of create post component', () => {
+  it('shows the preview button as disabled', () => {
     // get the preview button
-    expect(screen.getByTestId("previewButton")).toBeDisabled();
+    expect(screen.getByTestId('previewButton')).toBeDisabled();
   });
 
-  it("should enable the preview button after providing required fields", () => {
+  it('should enable the preview button after providing required fields', () => {
     // input title
-    const titleTextField = screen.getByPlaceholderText("title");
-    fireEvent.change(titleTextField, { target: { value: "Test Club" } });
+    const titleTextField = screen.getByPlaceholderText('title');
+    fireEvent.change(titleTextField, { target: { value: 'Test Club' } });
     // input body
-    const bodyTextField = screen.getByPlaceholderText("description");
-    fireEvent.change(bodyTextField, { target: { value: "body" } });
+    const bodyTextField = screen.getByTestId('bodyTextField');
+    fireEvent.change(bodyTextField, {
+      target: { value: 'This should be at least 25 characters long' },
+    });
     fireEvent.blur(bodyTextField);
-    expect(screen.getByTestId("previewButton")).not.toBeDisabled();
+    expect(screen.getByTestId('previewButton')).not.toBeDisabled();
   });
 
-  it("renders `previewPopUp` component upon clicking Preview", () => {
+  it('renders `previewPopUp` component upon clicking Preview', () => {
     // input title
-    fireEvent.change(screen.getByPlaceholderText("title"), {
-      target: { value: "Test Club" },
+    fireEvent.change(screen.getByPlaceholderText('title'), {
+      target: { value: 'Test Club' },
     });
     // input body
-    const bodyTextField = screen.getByPlaceholderText("description");
-    fireEvent.change(bodyTextField, { target: { value: "body" } });
+    const bodyTextField = screen.getByTestId('bodyTextField');
+    fireEvent.change(bodyTextField, {
+      target: { value: 'This should be at least 25 characters long' },
+    });
     fireEvent.blur(bodyTextField);
     // pop up should render
-    screen.getByTestId("previewButton").click();
-    expect(screen.getByTestId("PreviewPopUpComponent")).toBeInTheDocument();
+    screen.getByTestId('previewButton').click();
+    expect(screen.getByTestId('PreviewPopUpComponent')).toBeInTheDocument();
+  });
+
+  it('closes the dialog when clicked on `Back` button', () => {
+    expect(screen.getByTestId('newPostButton')).toBeInTheDocument();
   });
 });

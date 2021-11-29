ff0bc8793aa06b1de66d0f532098564c29a8ea88
danielduong8 <daniel.duong@mail.utoronto.ca>
2021-11-29 17:39:40 -0500

    Add client routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -79,7 +79,8 @@ function App() {
         <Routes>
           <Route path='/' element={<AuthContainer />} />
           <Route path='/dashboard' element={<ProtectedRoute />} />
-          <Route path='/api/v1/users/password-reset' element={<PassResetContainer />} /> {/* make sure this matches with conf email */}
+          <Route path='/password-reset' element={<PassResetContainer />} /> {/* make sure this matches with conf email */}
+          <Route path='/email-confirmation' element={<PassResetContainer />} /> {/* make sure this matches with conf email */}
         </Routes>
       </BrowserRouter>
     </ThemeProvider>

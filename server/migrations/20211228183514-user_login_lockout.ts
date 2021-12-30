a15e11da8fa8a0ef61d0806ee19c7cd135452f8d
Andy Chrzaszcz <andy9775@gmail.com>
2021-12-30 13:39:19 -0500

    Limit the maximum number of failed login attempts for a user. (#86)
    * Limit the maximum number of failed login attempts for a user.
    
    After a specified number of failed login attempts, the user is locked out for a defined period of
    time. Once the time has passed, the user can attempt to login again.
    
    closes: 12
    
    * remove mockdate

---
+++ server/migrations/20211228183514-user_login_lockout.ts
@@ -0,0 +1,29 @@
+import { Sequelize, QueryInterface, DataTypes } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, _: Sequelize) => {
+    await queryInterface.addColumn(
+      'Users', // table name
+      'can_login_after', // new field name
+      {
+        type: DataTypes.DATE,
+        allowNull: true,
+      }
+    );
+
+    await queryInterface.addColumn(
+      'Users', // table name
+      'failed_login_attempts', // new field name
+      {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        defaultValue: 0,
+      }
+    );
+  },
+
+  down: async (queryInterface: QueryInterface, _: Sequelize) => {
+    await queryInterface.removeColumn('Users', 'can_login_after');
+    await queryInterface.removeColumn('Users', 'failed_login_attempts');
+  },
+};
